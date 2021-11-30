456c858a7b48d447d10e1fe1ac396252b26f4f56
cjmayo <cjmayo@users.noreply.github.com>
2021-11-30 19:30:43 +0000

    Merge pull request #574 from cjmayo/nl_NL
    Add Dutch translation 2bc0b716bcc5206188aea59d7063fce3c040dee1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1+g606472e',
+    VERSION: '0.0.post1+g2bc0b71',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
