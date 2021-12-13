1b4206caa86d00eb05163eecf5c22ab5213dc2fb
cjmayo <cjmayo@users.noreply.github.com>
2021-12-13 19:24:37 +0000

    Merge pull request #585 from cjmayo/scm_git_archive
    Make installing from the git archive of a tagged commit possible 96f207d25fd87bf233b56eb4e60519da440bfe5e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.0.1.post77+g01dfc138',
+    VERSION: '10.0.1.post80+g96f207d2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
