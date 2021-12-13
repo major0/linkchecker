76671c9d68658fb27a7dcc06a6ea2e79aef61de7
cjmayo <cjmayo@users.noreply.github.com>
2021-12-13 19:26:32 +0000

    Merge pull request #587 from cjmayo/util-actions
    Workflows to automate updating man pages and create distribution files 2e9f08748d94738bf2ad65cf00496475445d97fb

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.0.1.post80+g96f207d2',
+    VERSION: '10.0.1.post83+g2e9f0874',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
