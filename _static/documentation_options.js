8a17c7e0d22c49303166cfed3f68cb1dd3e963e0
cjmayo <cjmayo@users.noreply.github.com>
2021-12-13 19:31:29 +0000

    Merge pull request #589 from cjmayo/proxy
    Remove linkcheck.checker.proxysupport 7f175c13d49f0d094c8e8111b91af08555381f0c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.0.1.post83+g2e9f0874',
+    VERSION: '10.0.1.post91+g7f175c13',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
