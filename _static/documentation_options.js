9458c588e66537ef40d2c5f05a158e5a33839ed8
cjmayo <cjmayo@users.noreply.github.com>
2021-12-15 19:32:10 +0000

    Merge pull request #590 from cjmayo/guess
    Don't guess the URL for files that end in .html 4b6f3c38b38f0263990ab476df93f46bf7156188

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.0.1.post91+g7f175c13',
+    VERSION: '10.0.1.post93+g4b6f3c38',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
