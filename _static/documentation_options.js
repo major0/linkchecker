c71d25366e7b18375e1d339adce5e1edacc8a860
cjmayo <cjmayo@users.noreply.github.com>
2021-12-20 19:26:58 +0000

    Merge pull request #599 from cjmayo/sitemap
    Use sphinx-sitemap to create a sitemap 58adec0a2829594fa15836061fe61eb1c2143786

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.0.1.post112+g366dcc14',
+    VERSION: '10.0.1.post114+g58adec0a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
