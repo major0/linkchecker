007d53a36306dd57fd77887f68f46e39a1af1ea5
cjmayo <cjmayo@users.noreply.github.com>
2021-12-21 19:41:21 +0000

    Merge pull request #601 from cjmayo/template
    Remove linkchecker from POSOURCES a12fcf04b2fc12dad743e49f641dbb36bc8dff9a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.0.1.post122+gbfee7835',
+    VERSION: '10.0.1.post124+ga12fcf04',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
