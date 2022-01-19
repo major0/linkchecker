bf865649f08a7d50cdf2a46006cf5289046be260
cjmayo <cjmayo@users.noreply.github.com>
2022-01-19 19:32:01 +0000

    Merge pull request #604 from cjmayo/py37
    Use metadata instead of _LinkChecker_configdata.py 5ec0aec11b568d9c0be6bc4f3fc5ce0c16a73495

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.0.1.post132+gd1078ed8',
+    VERSION: '10.1.0.post20+g5ec0aec1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
