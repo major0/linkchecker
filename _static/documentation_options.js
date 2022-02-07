b7731938bf9f332e0aef8e47837e00ea03a24afe
cjmayo <cjmayo@users.noreply.github.com>
2022-02-07 19:26:31 +0000

    Merge pull request #613 from JanMalte/update-schemes
    Add Telegram and WhatsApp schemes 9abd7d84afac4b6d974b9aa4fe0e12b40cd7cdce

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post25+gff7e157a',
+    VERSION: '10.1.0.post29+g9abd7d84',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
